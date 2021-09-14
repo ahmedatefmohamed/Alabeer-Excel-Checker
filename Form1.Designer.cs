ed4aaaff2a4295ce32c947fb6c972b73b59f8ca6
Ahmed Atef <43454769+ahmedatefmohamed@users.noreply.github.com>
2021-09-15 00:10:01 +0200

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace AlabeerExcelCheckerv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtProAttach = new System.Windows.Forms.TextBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtFileCount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monaco", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(550, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AlAbeer Excel Checker";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(477, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Provider Attatchment";
+            // 
+            // txtProAttach
+            // 
+            this.txtProAttach.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProAttach.Location = new System.Drawing.Point(479, 185);
+            this.txtProAttach.Name = "txtProAttach";
+            this.txtProAttach.Size = new System.Drawing.Size(409, 24);
+            this.txtProAttach.TabIndex = 2;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnUpload.Location = new System.Drawing.Point(894, 180);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 31);
+            this.btnUpload.TabIndex = 3;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = false;
+            this.btnUpload.Click += new System.EventHandler(this.btn_upload);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PaleGreen;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(479, 220);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Group";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btn_group);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightSalmon;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(571, 220);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 34);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Export";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.btn_export);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(476, 339);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(225, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Post-Grouped Attatchment";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(480, 374);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(488, 251);
+            this.listBox1.TabIndex = 7;
+            // 
+            // txtFileCount
+            // 
+            this.txtFileCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFileCount.Location = new System.Drawing.Point(894, 337);
+            this.txtFileCount.Name = "txtFileCount";
+            this.txtFileCount.Size = new System.Drawing.Size(72, 24);
+            this.txtFileCount.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(811, 342);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Files Count.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 570);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFileCount);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.txtProAttach);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Group Excel file";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtProAttach;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtFileCount;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace AlabeerExcelCheckerv2
+{
+    public partial class Form1 : Form
+    {
+        string filePath = string.Empty;
+        OpenFileDialog file = new OpenFileDialog();
+        string fileExtention = string.Empty;
+
+        Excel.Application excelApp;
+        Excel.Workbook xlWorkBook;
+        Excel.Worksheet xlWorkSheet;
+        Excel.Range range, // All sheet range.
+                    sortedRange, // range for column should sort to.
+                    hiddenRange; // range for columns want to hide.
+
+        public void sharedProcess()
+        {
+            filePath = file.FileName; //get the path of the file ex.(D\sheet.xlsx) 
+            fileExtention = Path.GetExtension(filePath); //get the file extension ex.(.xlsx)
+            String fileName = Path.GetFileNameWithoutExtension(filePath);
+
+            excelApp = new Excel.Application();
+            xlWorkBook = excelApp.Workbooks.Open(filePath); // open excel file.
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1); // get and read the sheet number 1.
+
+            int rowsCount = xlWorkSheet.UsedRange.Rows.Count - 2; // except header row {13} and total row @the last bottom.
+            int columnsCount = xlWorkSheet.UsedRange.Columns.Count; // get number of columns.
+
+            /** MessageBox.Show("Rows count: " + rowsCount); **/
+
+            range = xlWorkSheet.Range[$"A14:A{rowsCount}", "AF13"];
+            sortedRange = xlWorkSheet.Range[$"D14:D{rowsCount}"];
+            hiddenRange = xlWorkSheet.Range[$"G14:G{rowsCount}", "Z13"];
+
+            // Column index will be sum (Net {27}, VAT {28} and Billed {29})
+            int[] arr = new int[] { 27, 28, 29 };
+
+            hiddenRange.EntireColumn.Hidden = true; // hide unwanted range columns.
+
+            // Sort Insured Company Name Column...
+            sortedRange.Select();
+            xlWorkSheet.Sort.SortFields.Clear();
+            xlWorkSheet.EnableAutoFilter = true;
+            xlWorkSheet.AutoFilter.Sort.SortFields.Add(sortedRange, Excel.XlSortOn.xlSortOnValues, Excel.XlSortOrder.xlAscending, System.Type.Missing, Excel.XlSortDataOption.xlSortNormal);
+            xlWorkSheet.AutoFilter.ApplyFilter();
+            xlWorkSheet.Sort.SetRange(sortedRange);
+            xlWorkSheet.Sort.Header = Excel.XlYesNoGuess.xlGuess;
+            xlWorkSheet.Sort.MatchCase = false;
+            xlWorkSheet.Sort.Orientation = (XlSortOrientation)Excel.Constants.xlTopToBottom;
+            xlWorkSheet.Sort.SortMethod = Excel.XlSortMethod.xlPinYin;
+            xlWorkSheet.Sort.Apply();
+
+            // Grouped by Company Name
+            range.Subtotal(4, Excel.XlConsolidationFunction.xlSum, new int[] { 27, 28, 29 }, true, true, Excel.XlSummaryRow.xlSummaryBelow); ;
+            range.Activate();
+        }
+
+        public void groupExcel(bool isExport) {
+
+            if(isExport == true)
+            {
+                try
+                {
+                    sharedProcess();
+
+                    // Open Excel sheet and enable visibility.
+                    excelApp.Visible = true;
+
+                    xlWorkBook.Close(true, null, null);
+                    excelApp.Quit();
+
+                    releaseObject(excelApp);
+                    releaseObject(xlWorkBook);
+                    releaseObject(xlWorkSheet);
+                }
+                catch (Exception ex)
+                {
+                    // MessageBox.Show(ex.Message.ToString() + " Error!");
+                }
+            } 
+            else
+            {
+                String fileName = Path.GetFileNameWithoutExtension(filePath);
+                if (listBox1.Items.Contains(fileName) == true)
+                {
+                    MessageBox.Show("Already done!");
+                }
+                else
+                {
+                    try
+                    {
+                        sharedProcess();
+                        listBox1.Items.Add(fileName);
+                        txtFileCount.Text = listBox1.Items.Count.ToString();
+
+                        // Open Excel sheet and enable visibility.
+                        excelApp.Visible = true;
+
+                        xlWorkBook.Close(true, null, null);
+                        excelApp.Quit();
+
+                        releaseObject(excelApp);
+                        releaseObject(xlWorkBook);
+                        releaseObject(xlWorkSheet);
+                    }
+                    catch (Exception ex)
+                    {
+                        // MessageBox.Show(ex.Message.ToString() + " Error!");
+                    }
+                }
+            }
+
+            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Firing Upload event
+        /// </summary>
+        private void btn_upload(object sender, EventArgs e)
+        {
+            if (file.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filePath = file.FileName; //get the path of the file ex.(D\sheet.xlsx) 
+                fileExtention = Path.GetExtension(filePath); //get the file extension ex.(.xlsx)
+
+                if (fileExtention.CompareTo(".xls") == 0 || fileExtention.CompareTo(".xlsx") == 0)
+                {
+                    try { txtProAttach.Text = filePath; }
+                    catch (Exception ex) { MessageBox.Show(ex.Message.ToString()); }
+                }
+                else
+                {
+                    //dispaly a custom messageBox to show error  
+                    MessageBox.Show("Please choose .xls or .xlsx file only.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+                }
+            }
+        }
+
+        /// <summary>
+        /// Firing Group event
+        /// </summary>
+        private void btn_group(object sender, EventArgs e)
+        {
+            groupExcel(false);
+        }
+
+        /// <summary>
+        /// Firing Export event
+        /// </summary>
+        private void btn_export(object sender, EventArgs e)
+        {
+            groupExcel(true);
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Unable to release the Object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+    }
+}
